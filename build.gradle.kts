956ab6c4fb84319048a96830dc331fcd39145b13
Michal Vojáček <mvojacekdev@gmail.com>
2023-07-17 12:13:41 +0200

    build system

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.api.internal.artifacts.dsl.dependencies.DependenciesExtensionModule.module
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "eu.mvojacek.paper"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    shadow("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.shadowJar {
+    relocate("kotlin", "eu.mvojacek.paper.containerpassthrough.shadow.kotlin")
+    relocate("org.jetbrains.annotations", "eu.mvojacek.paper.containerpassthrough.shadow.org.jetbrains.annotations")
+    relocate("org.intellij.lang.annotations", "eu.mvojacek.paper.containerpassthrough.shadow.org.intellij.lang.annotations")
+    minimize()
+}
---
+++ src/main/kotlin/eu/mvojacek/paper/containerpassthrough/ContainerPassthroughNeo.kt
@@ -0,0 +1,13 @@
+package eu.mvojacek.paper.containerpassthrough
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ContainerPasthroughNeo : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
